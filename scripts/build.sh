775e5cec9851927aaa9279c4d6e00745e6a80ff7
Mitchell Mosure <hctimit@gmail.com>
2019-09-27 15:44:35 -0500

    fix: frozen library build error

--- scripts/build.sh
+++ scripts/build.sh
@@ -7,4 +7,4 @@ cd ${DIR}/../src
 gcc -c ./frozen-master/frozen.c -I ./frozen-master -o frozen.o
 ar rcs libfrozen.a frozen.o
 
-gcc main.c -o main.o -lsoundio -laubio -lm -I ./rpi_ws281x -L ./rpi_ws281x -lfrozen -lws2811 -Ofast
+gcc main.c -o main.o -lsoundio -laubio -lm -I ./rpi_ws281x -L ./rpi_ws281x -l:libfrozen.a -lws2811 -Ofast
