305b8b77008d68e48244f46974466cfd47718df9
Mitchell Mosure <hctimit@gmail.com>
2019-09-27 14:55:42 -0500

    feat: added frozen json parser dependency

--- src/build.sh
+++ src/build.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
-gcc main.c -o main.o -lsoundio -laubio -lm -I ./rpi_ws281x -L ./rpi_ws281x -lws2811 -Ofast
+gcc -c ./frozen-master/frozen.c -I ./frozen-master -o frozen.o
+ar rcs libfrozen.a frozen.o
+
+gcc main.c -o main.o -lsoundio -laubio -lm -I ./rpi_ws281x -L ./rpi_ws281x -lfrozen -lws2811 -Ofast
