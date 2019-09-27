9f89fb5deeb159fd8c55669e298a941408c686c4
Mitchell Mosure <hctimit@gmail.com>
2019-09-26 23:35:22 -0500

    feat: added smee.io webhook on repository

--- api/src/app.ts
+++ api/src/app.ts
@@ -1,3 +1,5 @@
+import { SmeeClient } from 'smee-client';
+
 import express from 'express';
 
 const app = express();
@@ -7,9 +9,26 @@ app.get('/', (request, response) => {
     response.send('Status endpoint');
 });
 
+// Push webhook
+app.post('/push', (request, response) => {
+    // TODO: Implement config.json write from body and git-refresh.sh
+});
+
 app.listen(port, (err) => {
     if (err) {
         return console.error(err);
     }
     return console.log(`server is listening on ${port}`);
 });
+
+// Start a smee.io forwarder for GitHub webhooks
+const smee = new SmeeClient({
+    logger: console,
+    source: 'https://smee.io/RFydymJ4LceIVeNu',
+    target: 'http://localhost:' + port + '/push',
+});
+
+const events = smee.start();
+
+// Stop forwarding events
+events.close();
