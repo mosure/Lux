e0fe3be739d15d0a3304c5830a13b6dcbbc00e2f
Mitchell Mosure <hctimit@gmail.com>
2019-11-11 17:57:15 -0600

    refactor: script cleanup

--- scripts/git-refresh.sh
+++ scripts/git-refresh.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
 
 readonly DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-readonly TASK_NAME="Lux"
-
-# Kill the current task
-if pgrep "$TASK_NAME"; then pkill -f "$TASK_NAME"; fi
 
 # Reset to origin master
 git fetch --all
@@ -14,4 +10,4 @@ git reset --hard origin/master
 # Build the new C Application from Source
 "${DIR}/build.sh"
 
-bash -c "exec -a $TASK_NAME ${DIR}/start.sh &"
+"${DIR}/restart.sh"
--- scripts/start.sh
+++ scripts/start.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
 readonly DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+readonly TASK_NAME="Lux"
 
-${DIR}/../src/main.o
+exec -a $TASK_NAME ${DIR}/../src/main.o
