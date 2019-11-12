5870a01c54df5e0e2935cd852e38bbb900d6af3c
Mitchell Mosure <hctimit@gmail.com>
2019-11-11 20:20:52 -0600

    another thing

--- src/main.c
+++ src/main.c
@@ -686,8 +686,8 @@ void bouncer(int channel, double frame, int frame_hops)
         shift_origin(channel, frame_hops);
     }
 
-    if (frame > 0 && ((int)(frame / 15)) % 10 == 0) {
-        set_led_origin(channel, 0, (struct HSV){ .H = map((int)(frame / 15) % 15, 0, 15, 0, 1), .S = 1, .V = 1 });
+    if (frame > 0 && ((int)(frame / 15)) % 12 == 0) {
+        set_led_origin(channel, 0, (struct HSV){ .H = map((int)(frame / 15) % 21, 0, 21, 0, 1), .S = 1, .V = 1 });
     }
 }
 
