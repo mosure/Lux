73c35756508e402b173467eb7cb3ff7500374046
Mitchell Mosure <hctimit@gmail.com>
2019-11-11 19:56:21 -0600

    feat: larger local dim collector

--- src/main.c
+++ src/main.c
@@ -95,7 +95,7 @@ const uint_t hop_s = 256;
 const uint_t n_filters = 40;
 const uint_t n_pitch_acc = 400;
 const uint_t n_energy_mag_acc = 100;
-const uint_t n_local_dim = 500;
+const uint_t n_local_dim = 1000;
 const uint_t max_buffer_runs = 400;
 uint_t samplerate;
 
@@ -715,7 +715,7 @@ int main(int argc, char *argv[])
         float energy_mag_acc_mean = fvec_mean(energy_mag_acc);
         float energy_local_dim_mean = fvec_mean(local_dim_buf);
 
-        double lightness = map(energy_mag_acc_mean / (0.8 * energy_local_dim_mean + 0.001), 0.0005, 1, 0, 1);
+        double lightness = map(energy_mag_acc_mean / (energy_local_dim_mean + 0.001), 0.0005, 1.5, 0, 1);
 
         float pitch_acc_mean = fvec_mean(pitch_acc);
 
