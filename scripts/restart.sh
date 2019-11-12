a303ddf512c45f0966b835acce98dcaf5b346e0e
Mitchell Mosure <hctimit@gmail.com>
2019-11-11 19:11:03 -0600

    fix: rename build output

--- scripts/restart.sh
+++ scripts/restart.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 readonly DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-readonly TASK_NAME="Lux"
+readonly TASK_NAME="Lux.o"
 
 # Kill the current task
 if pgrep "$TASK_NAME"; then pkill -f "$TASK_NAME"; fi
--- scripts/start.sh
+++ scripts/start.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 readonly DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-readonly TASK_NAME="Lux"
+readonly TASK_NAME="Lux.o"
 
 exec -a $TASK_NAME ${DIR}/../src/main.o
