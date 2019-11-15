e6e564bae345d28124cc888df480d93afbc3ac06
Mitchell Mosure <hctimit@gmail.com>
2019-11-14 18:17:31 -0600

    hmmm

--- src/main.c
+++ src/main.c
@@ -772,7 +772,7 @@ int main(int argc, char *argv[])
         float energy_mag_acc_mean = fvec_mean(energy_mag_acc);
         float energy_local_dim_mean = fvec_mean(local_dim_buf);
 
-        double lightness = map(energy_mag_acc_mean - 0.75 * energy_local_dim_mean), 0.0005, 1.5, 0, 1);
+        double lightness = map(energy_mag_acc_mean - 0.75 * energy_local_dim_mean, 0.0005, 1.5, 0, 1);
 
         float pitch_acc_mean = fvec_mean(pitch_acc);
 
